e7a7751b1a2f5a56d9e5815c4223842eb314050f
Remi Collet <fedora@famillecollet.com>
2012-11-13 11:21:41 +0100

    add License header

--- fcgiget.php
+++ fcgiget.php
@@ -1,5 +1,27 @@
 #!/usr/bin/php
 <?php
+/**
+ * Note : Code is released under the GNU LGPL
+ *
+ * Please do not change the header of this file
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ */
+
+/**
+ * Simple command line script to test communication with a FastCGI server
+ *
+ * @author      Pierrick Charron <pierrick@webstart.fr>
+ * @author      Remi Collet <remi@famillecollet.com>
+ * @version     1.0
+ */
 require('fastcgi.php');
 
 if (!isset($_SERVER['argc'])) {
